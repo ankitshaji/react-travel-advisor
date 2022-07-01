56fdfcee0bd339ec039226867dc91ad888444240
Ankit Shaji <ankitshaji@gmail.com>
2022-07-01 16:42:24 +1200

    creating makeStyles function to edit css classes

---
+++ src/components/Header/styles.js
@@ -0,0 +1,53 @@
+//materia-ui components - ie google material design
+//import alhpa(colors) and makeStyles functions from styles
+import { alpha, makeStyles } from "@material-ui/core/styles";
+
+//makeStyles function takes parameter object with css classes
+//create a callback function in makeStyles that returns object with css classes
+//makeStyles gives us theme object we can use in callback function
+//export makestyles ie react hook
+export default makeStyles((theme) => ({
+  //using theme and alpha
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: { width: "20ch" },
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
